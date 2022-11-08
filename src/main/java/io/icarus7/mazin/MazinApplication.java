44b08c76c0ef71aee5023f443ffaf890ca3e0146
mohkelmohamady <moh.k.elmohamady@gmail.com>
2022-11-09 00:55:21 +0100

    First commit, initialized repository and added README file

---
+++ src/main/java/io/icarus7/mazin/MazinApplication.java
@@ -0,0 +1,13 @@
+package io.icarus7.mazin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MazinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MazinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/icarus7/mazin/MazinApplicationTests.java
@@ -0,0 +1,13 @@
+package io.icarus7.mazin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
